2db360c803c931307f0db3fa91dca3b4c5302126
liyu <1170667320@qq.com>
2022-10-25 23:34:37 +0800

    feat<coupon-template-serv>:add basic coupon-template-serv api

---
+++ coupon-template-serv/coupon-template-api/src/main/java/com/liyu/coupon/template/api/beans/PagedCouponTemplateInfo.java
@@ -0,0 +1,28 @@
+package com.liyu.coupon.template.api.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+//import javax.validation.constraints.NotNull;
+
+/**
+ * 创建优惠券模板
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PagedCouponTemplateInfo {
+
+    public List<CouponTemplateInfo> templates;
+
+    public int page;
+
+    public long total;
+
+}
